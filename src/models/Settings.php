5482e7d32a0d33915c77620dfd060d5f5139f811
Mark Huot <mark@markhuot.com>
2023-04-02 10:22:45 -0400

    Update Settings.php

--- src/models/Settings.php
+++ src/models/Settings.php
@@ -4,6 +4,7 @@ namespace markhuot\craftai\models;
 
 use Illuminate\Support\Arr;
 use markhuot\craftai\db\Model;
+use markhuot\craftai\search\NullSearch;
 
 class Settings extends Model
 {
@@ -11,17 +12,17 @@ class Settings extends Model
      * Whether the system should reach out to the various AI services or
      * rely on fakes.
      */
-    public bool $useFakes;
+    public bool $useFakes = false;
 
     /**
      * The driver to use for the AI services.
      */
-    public string $searchDriver;
+    public string $searchDriver = 'null';
 
     /**
      * @var array The configuration for the various search back-ends
      */
-    public array $searchDrivers;
+    public array $searchDrivers = ['null' => ['class' => NullSearch::class]];
 
     public function get($key)
     {

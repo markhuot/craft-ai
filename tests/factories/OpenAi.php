3ae957252a3281743236b78ce3b0d0853e7d6424
markhuot <mark@markhuot.com>
2023-04-02 20:17:29 -0400

    pest fixes

--- tests/factories/OpenAi.php
+++ tests/factories/OpenAi.php
@@ -3,7 +3,12 @@
 namespace markhuot\craftai\tests\factories;
 
 use markhuot\craftai\backends\OpenAi as OpenAiModel;
+use markhuot\craftai\features\Chat;
 use markhuot\craftai\features\Completion;
+use markhuot\craftai\features\EditImage;
+use markhuot\craftai\features\EditText;
+use markhuot\craftai\features\GenerateEmbeddings;
+use markhuot\craftai\features\GenerateImage;
 use markhuot\craftpest\factories\Factory;
 
 class OpenAi extends Factory
@@ -17,7 +22,12 @@ class OpenAi extends Factory
                 'baseUrl' => 'https://api.openai.com/v1/',
                 'apiKey' => '$OPENAI_API_KEY',
                 'enabledFeatures' => [
-                    Completion::class
+                    Chat::class,
+                    Completion::class,
+                    EditImage::class,
+                    EditText::class,
+                    GenerateEmbeddings::class,
+                    GenerateImage::class,
                 ],
             ],
         ];

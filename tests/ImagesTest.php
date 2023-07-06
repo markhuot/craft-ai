00245afbecda6ec53623a69be125be220aec93b1
markhuot <mark@markhuot.com>
2023-07-06 15:01:30 -0400

    tests

--- tests/ImagesTest.php
+++ tests/ImagesTest.php
@@ -4,9 +4,11 @@ use markhuot\craftpest\factories\User;
 
 it('generates images', function () {
     $user = User::factory()->admin(true)->create();
+    $backend = \markhuot\craftai\backends\OpenAi::factory()->create();
     $volume = \markhuot\craftpest\factories\Volume::factory()->create();
 
     $this->actingAs($user)->action('ai/image/store-generation', [
+        'backend' => $backend->id,
         'prompt' => 'two otters holding hands',
         'volume' => $volume->id,
     ])
